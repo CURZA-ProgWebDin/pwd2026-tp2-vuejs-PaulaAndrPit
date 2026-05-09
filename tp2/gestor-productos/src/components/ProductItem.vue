cf2e502c35137e53565e0a2df688b7c6b068d605
PaulaAndrPit <paulaandreapitto@gmail.com>
2026-05-09 16:57:24 -0300

    Entrega tp2

---
+++ tp2/gestor-productos/src/components/ProductItem.vue
@@ -0,0 +1,54 @@
+<template>
+  
+  <li>
+    <div class="item">
+      <strong>{{ producto.nombre }}: </strong>
+              Precio: {{ producto.precio }}
+              Stock: {{ producto.stock }}
+              Categoría: {{ producto.categoria }}
+  
+
+      <button @click="eliminar" class="boton2">Eliminar</button>
+    </div>
+  </li>
+  
+</template>
+
+
+<script setup>
+const props = defineProps({
+  producto: Object
+})
+
+const emit = defineEmits(['eliminar'])
+
+function eliminar() {
+  emit('eliminar', props.producto.id)
+}
+</script>
+
+
+
+<style scoped>
+
+.boton2 {
+  background-color: rgb(45, 45, 184);
+  color: white;
+  border: none;
+  border-radius: 5px;
+  padding: 5px 10px;
+  cursor: pointer;
+}
+.boton2:hover {
+  background-color: rgb(3, 3, 83);
+}
+
+.item strong {
+  font-size: 18px;
+  text-transform: uppercase;
+}
+</style>
+
+
+
+
---
+++ tp2/gestor-productos/src/components/ProductList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h3>Lista de Productos:</h3>
+
+    <div v-if="props.productos.length === 0">
+      No hay productos en esta categoría.
+    </div>
+
+    <ul>
+      <ProductItem
+        v-for="producto in props.productos"
+        :key="producto.id"
+        :producto="producto"
+        @eliminar="eliminar"
+      />
+    </ul>
+  </div>
+</template>
+
+
+<script setup>
+import { reactive } from 'vue'
+import ProductItem from './ProductItem.vue'
+
+
+const props = defineProps({
+  productos: Array
+})
+
+const emit = defineEmits(['eliminar'])
+
+
+function eliminar(id) {
+  emit('eliminar', id)
+}
+</script>
+
+<style scoped>
+h3 {
+  margin-top: 10px;
+  text-align: left;
+  text-decoration: underline;
+  color: rgb(45, 45, 184);
+}
+
+
+</style>
